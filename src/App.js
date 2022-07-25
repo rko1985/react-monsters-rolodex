c62ed8f07db32852f9e477f5871c70ea2fb98758
Russell Oyama <russelloyama@gmail.com>
2022-07-25 08:12:31 -1000

    set state secondary callback

--- src/App.js
+++ src/App.js
@@ -24,9 +24,11 @@ class App extends Component {
             Hello {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
           </p>
           <button onClick={ () => {
-            this.setState({ name: {firstName: 'Andrei', lastName: 'Neaogie'} });
-            console.log(this.state);
-
+            this.setState(() => {
+              return {name: {firstName: 'Andrei', lastName: 'Neaogie'},
+                };
+              }
+            );
           }}>
             Change Name
           </button>
