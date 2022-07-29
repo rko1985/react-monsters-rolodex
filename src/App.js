44214faf2d07f2c97f4afc25bc2baba283a260c3
Russell Oyama <russelloyama@gmail.com>
2022-07-29 06:52:03 -1000

    search box

--- src/App.js
+++ src/App.js
@@ -11,27 +11,31 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor');
   }
 
   componentDidMount(){
+    console.log('component did mount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return {monsters: users};
-        },
-        () => {
-          console.log(this.state);
-        }
-      )
-    );
+      .then((users) => 
+        this.setState(
+          () => {
+            return {monsters: users};
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
         
   }
 
   render () {
-
+    console.log('render');
     return (
       <div className="App">
-        
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {console.log(event.target.value)}}/>
         {
           this.state.monsters.map((monster) => {
             return <div key={monster.id}>
