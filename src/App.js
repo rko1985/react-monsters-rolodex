bd3b8ae0fbeebcb30b926d794638a2ff657bfd4e
Russell Oyama <russelloyama@gmail.com>
2022-08-01 11:51:24 -1000

    searching and filtering

--- src/App.js
+++ src/App.js
@@ -35,7 +35,18 @@ class App extends Component {
     console.log('render');
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {console.log(event.target.value)}}/>
+        <input className='search-box' type='search' placeholder='search monsters' 
+          onChange={(event) => {
+            console.log(event.target.value)
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters};
+            });
+          }}/>
         {
           this.state.monsters.map((monster) => {
             return <div key={monster.id}>
