6f0ed6ed1803b34afded026ce6d9c2406832edd0
Russell Oyama <russelloyama@gmail.com>
2022-08-10 06:55:55 -1000

    css in react

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
         <SearchBox 
           onChangeHandler={onSearchChange} 
           placeholder='search-monsters' 
-          className='search-box'
+          className='monsters-search-box'
         />
 
         <CardList monsters={filteredMonsters}/>
