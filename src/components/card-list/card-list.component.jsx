00380ce7dd7b19cf3742e7d59915b794eee16b58
Russell Oyama <russelloyama@gmail.com>
2022-08-19 06:47:21 -1000

    converted remaining components into functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,28 +1,17 @@
-import { Component } from "react";
+
 
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
+const CardList = ({monsters}) => (
+    <div className='card-list'>
+        {monsters.map((monster) => {
 
-    render(){
-
-        // console.log(this.props.monsters);
-        // console.log('render from cardlist')
-        const {monsters} = this.props;
-       
-        return (
-            
-            <div className='card-list'>
-                {monsters.map((monster) => {
-
-                    return (
-                        <Card monster={monster}/>
-                )})}               
-                
-            </div>
-        );
-    }
-}
+            return (
+                <Card monster={monster}/>
+        )})}               
+        
+    </div>
+);
 
 export default CardList;
