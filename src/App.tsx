0654cc6f2761753484b0a12694c666de1184d8b0
Russell Oyama <russelloyama@gmail.com>
2023-01-02 09:57:08 -1000

    converting App.js to type script, remaining components converted to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 // import { Component  } from 'react';
 
-import { useState, useEffect } from 'react'; //hooks
+import { useState, useEffect, ChangeEvent } from 'react'; //hooks
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
+
+import { getData } from './utils/fetch.utils';
 import './App.css';
+
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+}
 
 const App = () => {
 
   
   const [searchField, setSearchField] = useState('a');
   const [title, setTitle] = useState('');
-  const [monsters, setMonsters] = useState([]);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [filteredMonsters, setFilterMonsters] = useState(monsters);
   
   
@@ -27,19 +35,22 @@ const App = () => {
   }, [monsters, searchField]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setMonsters(users)
-    );
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>(
+        'https://jsonplaceholder.typicode.com/users'
+        );
+      setMonsters(users);
+    };
+    fetchUsers();
   }, []);
 
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString)
     
   }
 
-  const onTitleChange = (event) => {
+  const onTitleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setTitle(searchFieldString)
     
--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,9 +1,13 @@
 
-
+import { Monster } from '../../App';
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-const CardList = ({monsters}) => (
+type CartListProps = {
+    monsters: Monster[]
+}
+
+const CardList = ({monsters}: CartListProps) => (
     <div className='card-list'>
         {monsters.map((monster) => {
 
